3eac80eb17a3af5c9c14da2624ca1b2561658307
Jason Hamm <jason.robert.hamm@gmail.com>
2024-08-18 13:51:30 -0400

    feat: setup lualine

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,78 @@
+return {
+	"nvim-lualine/lualine.nvim",
+	opts = {},
+	config = function()
+		require("lualine").setup({
+			options = {
+				-- theme = lualine_horizon,
+				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
+				-- disable lualine for dapui windows
+				disabled_filetypes = {
+					"dapui_watches",
+					"dapui_breakpoints",
+					"dapui_scopes",
+					"dapui_console",
+					"dapui_stacks",
+					"dap-repl",
+				},
+			},
+			sections = {
+				lualine_a = {
+					-- "branch"
+					"filename",
+				},
+				lualine_b = {
+					-- "branch",
+					-- "diff",
+					-- "diagnostics",
+				},
+				-- lualine_a = { "mode" },
+				-- lualine_b = {
+				-- 	"branch",
+				-- 	"diff",
+				-- 	"diagnostics",
+				-- },
+				lualine_c = {
+					--"filename",
+				},
+				-- lualine_x = { "progress" },
+				-- lualine_y = { "location" },
+				lualine_x = {
+					-- "diagnostics"
+				},
+				lualine_y = {
+					-- "diff"
+					-- "location"
+				},
+				lualine_z = {
+					"progress",
+					-- "filename",
+					-- "buffers"
+					-- "filetype",
+					-- "fileformat",
+					-- "encoding",
+				},
+			},
+			tabline = {
+				lualine_a = {
+					-- "branch"
+					{
+						"buffers",
+						symbols = {
+							-- alternate_file = "",
+							modified = " +",
+						},
+					},
+				},
+				-- 	lualine_a = { "filename" },
+				-- 	lualine_b = {},
+				-- 	lualine_c = {},
+				-- 	lualine_x = {},
+				-- 	lualine_y = { "" },
+				lualine_z = { "tabs" },
+				-- 	lualine_z = {},
+			},
+		})
+	end,
+}

d55abc5b0633ffe6076d16fbc38315811137249a
Jason Hamm <jason.robert.hamm@gmail.com>
2024-12-23 20:55:35 -0500

    refactor: change colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,37 @@
 return {
-	-- "rose-pine/neovim",
-    "EdenEast/nightfox.nvim",
+	"rose-pine/neovim",
+    -- "catppuccin/nvim",
+    -- "EdenEast/nightfox.nvim",
+    -- "loctvl842/monokai-pro.nvim",
+    -- "folke/tokyonight.nvim",
+    -- "akinsho/horizon.nvim",
+    -- "Mofiqul/dracula.nvim",
+
 	-- priority = 1000, -- Make sure to load this before all the other start plugins.
 	init = function()
-		-- vim.cmd.colorscheme("rose-pine")
+		vim.cmd.colorscheme("rose-pine")
 		-- vim.cmd.colorscheme("nightfox")
 		-- vim.cmd.colorscheme("carbonfox")
-        vim.cmd.colorscheme("terafox")
-
+        -- vim.cmd.colorscheme("terafox")
+        -- vim.cmd.colorscheme("horizon")
+        -- vim.cmd.colorscheme("monokai-spectrum")
+        -- vim.cmd.colorscheme("tokyonight-storm")
+        -- vim.cmd.colorscheme("dracula")
+		-- vim.cmd.colorscheme("catppuccin")
+--
 		-- You can configure highlights by doing something like:
 		vim.cmd.hi("Comment gui=none")
 	end,
+    -- config = function()
+        -- require("monokai-pro").setup({
+        --   filter = "octagon", -- classic | octagon | pro | machine | ristretto | spectrum
+        -- })
+        --
+        -- require("catppuccin").setup({
+        --     flavour = "mocha",
+        --     background = {
+        --         dark = "mocha"
+        --     }
+        -- })
+    -- end
 }
